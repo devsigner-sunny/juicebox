b6645c9b137cd66519901a91130ef6830b2b6ea4
devsigner-sunny <devsigner.sunny@gmail.com>
2024-08-15 22:17:15 +0800

    build error handling - useSearchParams

--- app/template.tsx
+++ app/template.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { Suspense, useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import SmoothScrolling from "@/components/SmoothScrolling";
 import gsap from "gsap";
 import { useMediaQuery } from "@/hooks/use-mediaquery";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     const handleRouteChange = () => {
@@ -15,12 +14,11 @@ export default function Template({ children }: { children: React.ReactNode }) {
       gsap.to(".template", { opacity: 1, duration: 0.5 });
     };
     handleRouteChange();
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   let isPageHeight = useMediaQuery("(min-height: 600px)");
 
   return (
-		<Suspense>
     <div
       className={`${
         isPageHeight ? "__main-min-screen-height" : "border-red-500"
@@ -29,6 +27,5 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <div className="absolute top-0 left-0 -z-[1] w-full h-full pointer-events-none bg-brand-gradient"></div>
       <SmoothScrolling>{children}</SmoothScrolling>
     </div>
-		</Suspense>
   );
 }
